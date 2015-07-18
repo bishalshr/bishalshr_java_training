f6d250a702b9308a0ac6100796845f9ccab52c33
stha <stha@bishal>
2015-07-18 14:38:39 +0545

    First program added to git

---
+++ src/com/bishal/java/training/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bishal.java.training;
+
+/**
+ * @author stha
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
